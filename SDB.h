f8f2d0e9aa61bce0006f08a1a8de1f1907c9bc0b
YousefOsamaa <Y_osama_atta@yahoo.com>
2023-08-24 00:13:58 +0300

    Files and data types creation

---
+++ SDB.h
@@ -0,0 +1,16 @@
+#include "STD.h"
+typedef struct SimpleDb
+{
+    uint32 Student_ID;
+    uint32 Student_Year;
+    uint32 Course1_ID;
+    uint32 Course1_Grade;
+    uint32 Course2_ID;
+    uint32 Course2_Grade;
+    uint32 Course3_ID;
+    uint32 Course3_Grade;
+
+
+}Student;
+
+//Functions Prototypes :
---
+++ STD.h
@@ -0,0 +1,9 @@
+
+// Data types declaration
+typedef unsigned char uint8;
+typedef char int8;
+typedef unsigned short uint16;
+typedef short int16;
+typedef unsigned int uint32;
+
+//enum Boolean {false, true} bool;
